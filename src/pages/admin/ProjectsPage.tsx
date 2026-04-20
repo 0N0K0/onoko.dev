1915a9ef64d03e8922ace510595e852aab35ef09
NKoelblen <hello@onoko.dev>
2026-04-20 14:58:24 +0200

    feat: enhance ProjectsPage to display client logo, start date, and categories in project list

--- src/pages/admin/ProjectsPage.tsx
+++ src/pages/admin/ProjectsPage.tsx
@@ -3,6 +3,9 @@ import useProjectMutations from "../../hooks/mutations/useProjectMutations";
 import useProjects from "../../hooks/queries/useProjects";
 import EntitiesContent from "../../layout/admin/EntitiesContent";
 import ProjectFormDialog from "../../components/entities/ProjectFormDialog";
+import type { Project } from "../../types/entities/projectTypes";
+import Picture from "../../components/custom/Picture";
+import type { Category } from "../../types/entities/categoryTypes";
 
 /**
  * Page d'administration pour la gestion des projets.
@@ -69,14 +72,43 @@ export default function Projects() {
         loading={loading}
         fields={[
           {
+            key: "client.logo",
+            label: "Logo",
+            content: (item: Project) =>
+              typeof item.client?.logo === "object" ? (
+                <Picture
+                  image={item.client.logo}
+                  maxHeight="48px"
+                  maxWidth="48px"
+                />
+              ) : null,
+          },
+          {
+            key: "client.label",
+            label: "Client",
+            content: (item: Project) => item.client?.label,
+          },
+          {
             key: "label",
             label: "Label",
-            content: (item) => (
-              <p style={{ paddingLeft: `${item.depth * 16}px` }}>
-                {item.depth > 0 ? `- ` : ""}
-                {item.label}
-              </p>
-            ),
+            content: (item: Project) => item.label,
+          },
+          {
+            key: "startDate",
+            label: "Date de début",
+            content: (item: Project) => item.startDate?.format("MM/YYYY"),
+          },
+          {
+            key: "categories",
+            label: "Catégories",
+            content: (item: Project) =>
+              item.categories
+                ?.filter(
+                  (category): category is Category =>
+                    typeof category === "object",
+                )
+                .map((category: Category) => category.label)
+                .join(", "),
           },
         ]}
         onClickActions={{
